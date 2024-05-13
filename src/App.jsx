035009de196c4e6a2b727bd890b58227a4e7b99f
Saif Rahman <srahman@powerley.com>
2024-05-13 20:22:03 +0600

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useRoutes } from "react-router-dom"
+import Home from "./pages/Home"
+
 function App() {
+  const routesArray = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]
+  let routesElement = useRoutes(routesArray)
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col min-h-screen w-full">
+      {/* Proper Header */}
+      <header>Header</header>
+
+      {/* Main Content */}
+      <main className="w-full flex flex-col flex-1 h-full">
+        {routesElement}
+      </main>
+
+      {/* Proper Footer */}
+      <footer>Footer</footer>
+    </div>
   )
 }
 
